40197e3fb61c32ff154e5edb0fb577ef4760661f
keuwey <keuwey@gmail.com>
2024-09-25 17:01:51 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #111e70;
+        color: white;
+        padding: 0 10px;
+        font-size: 25px;
+        font-family: "Roboto";
+    }
+`;
